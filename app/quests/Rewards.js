65dea39975ed826b771d1f45f0eafdee3a00abf8
vansh potpose <vanshpotpose@gmail.com>
2025-07-02 14:23:41 +0530

    quest page made responsive

--- app/quests/Rewards.js
+++ app/quests/Rewards.js
@@ -1,28 +1,34 @@
 import { BsCoin } from "react-icons/bs";
 import { IoCube } from "react-icons/io5";
+import { FaHeart, FaStar,FaAward } from "react-icons/fa";
 export default function Rewards({rewards}) {
-    return (    <div className="flex gap-5 items-center flex-wrap">
+    return (    <div className="flex gap-2 sm:gap-3 md:gap-5 items-center font-semibold flex-wrap">
                     {rewards.map((reward, index) => (
                       <div key={index} className="flex">
                         {reward.type === "coins" && (
-                          <span className="flex gap-1 text-md items-center">
-                            <BsCoin size={20} className="text-yellow-500" />
-                            {reward.data.amount}
+                          <span className="flex gap-1 text-xs sm:text-sm md:text-md items-center">
+                            +{reward.data.amount}
+                            <BsCoin size={16} className="text-yellow-500 sm:w-5 sm:h-5" />
                           </span>
                         )}
                         {reward.type === "experience" && (
-                          <span className="flex gap-1 text-md items-center text-blue-300">
-                            + {reward.data.amount} exp
+                          <span className="flex gap-1 text-xs sm:text-sm md:text-md items-center text-blue-400">
+                            +{reward.data.amount}             
+                            <FaStar  className="text-blue-400 mb-1" />
+                            
                           </span>
                         )}
                         {reward.type === "skill" && (
-                          <span className="flex gap-1 text-md items-center text-green-400">
-                            + {reward.data.amount} {reward.data.skill}
+                          <span className="flex gap-1 text-xs sm:text-sm md:text-md items-center text-green-400">
+                            +{reward.data.amount} 
+                                        <FaAward  className="text-green-400 " />
+
+                            {reward.data.skill}
                           </span>
                         )}
                         {reward.type === "item" && (
-                            <span className="flex gap-1 text-sm items-center">
-                          <IoCube size={20} className="text-blue-600" /> {reward.data.item.name}
+                            <span className="flex gap-1 text-xs sm:text-sm items-center">
+                          <IoCube size={16} className="text-blue-600 sm:w-5 sm:h-5" /> {reward.data.item.name}
                         </span>
                         )}
                       </div>
